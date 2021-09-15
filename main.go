f8ba0a218dc1e8e5f69f4db8782d6561a1300dcc
corazonnn <zero7364@icloud.com>
2021-09-15 16:03:41 +0900

    add: main.go http.Handle定義

--- main.go
+++ main.go
@@ -1,26 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	msg := "hello world"
-	fmt.Println(msg)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-		<html>
-	    <head>
-			  <title>チャット</title>	
-			</head>
-			<body>
-			  チャットしましょう！
-			</body>
-		</html>
-		`))
+//【目的】ファイルからテンプレートを作成し、データを出力する
+type templateHandler struct { //テンプレートの構造
+	once     sync.Once //一度だけ実行
+	filename string
+	templ    *template.Template //パースしたファイルの内容を入れる
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, u *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates",
+				t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	//webサーバーの開始
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
