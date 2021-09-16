9570b344854c96a708e1f581df4163f706fd2e56
corazonnn <zero7364@icloud.com>
2021-09-16 16:25:24 +0900

    add:ユニットテスト書いた

--- trace/tracer_test.go
+++ trace/tracer_test.go
@@ -1,10 +1,21 @@
 package trace
 
 import (
+	"bytes"
 	"testing"
 )
 
 //ユニットテスト
 func TestNew(t *testing.T) {
+	var buf bytes.Buffer //出力されるデータがbytes.Bufferに保持されている
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("newからの戻り値がnilです")
+	} else {
+		tracer.Trace("こんにちは、traceパッケージ")
+		if buf.String() != "こんにちは、traceパッケージ\n" {
+			t.Errorf("'%s'という誤った文字列が出力されました", buf.String())
+		}
+	}
 	t.Error("まだテストを作成していません")
 }
