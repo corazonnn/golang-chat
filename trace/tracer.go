f8a3e677452f4d15d85049c2989645c37a9000cc
corazonnn <zero7364@icloud.com>
2021-09-16 16:31:05 +0900

    add:最低限のユニットテストを通るようにした

--- trace/tracer.go
+++ trace/tracer.go
@@ -1,6 +1,10 @@
 package trace
 
+import "io"
+
 //Tracerはログを記録できるオブジェクトを表すインターフェース
 type Tracer interface {
 	Trace(...interface{}) //任意の引数の型を何個でも取れる
 }
+
+func New(w io.Writer) Tracer { return nil }
