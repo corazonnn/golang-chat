6a9358b1e9bc0dd1c655ac3104dd342b78b0d30e
corazonnn <zero7364@icloud.com>
2021-09-18 08:52:18 +0900

    add: auth雛形

---
+++ auth.go
@@ -0,0 +1,27 @@
+package main
+
+import "net/http"
+
+type authHandler struct {
+	next http.Handler //wrap用かな
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		//未認証
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+
+	} else if err != nil {
+		//なんらかの別のエラーが発生
+		panic(err.Error())
+	} else {
+		//成功。ラップされたハンドラを呼び出します
+		h.next.ServeHTTP(w, r)
+	}
+}
+
+//これわからん
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
